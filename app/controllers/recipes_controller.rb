d645fb642bc49081fe66c390899721abcb5f0a34
Daniyal Qamar <dev.daniyal.visnext@gmail.com>
2021-11-05 03:47:30 -0400

    added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,12 @@
 class RecipesController < ApplicationController
+	
+	before_action :set_recipe, only: [:show, :edit, :update]
+
 	def index
 		@recipes = Recipe.all
 	end
 
 	def show
-
-		@recipe = Recipe.find(params[:id])
 
 	end
 
@@ -26,12 +27,12 @@ class RecipesController < ApplicationController
 
 	#editing purposes
 	def edit
-		@recipe = Recipe.find(params[:id])
+		
 	end
 
 	#control updaing button (A post request)
 	def update
-		@recipe = Recipe.find(params[:id])
+		
 		if @recipe.update(recipe_params)
 			flash[:success] = "Recipe was updated successfully!"
 			redirect_to recipe_path(@recipe)
@@ -48,6 +49,10 @@ class RecipesController < ApplicationController
 	end
 
 	private
+
+		def set_recipe
+			@recipe = Recipe.find(params[:id])
+		end
 
 		def recipe_params
 			params.require(:recipe).permit(:name, :email)
